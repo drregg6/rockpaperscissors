8ed6af4ce88796cdd1935eebcab6cc5121b148e1
Dave <drregg6@gmail.com>
2018-02-23 16:32:43 -0500

    Updated js and css

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,77 +1,79 @@
+var select = document.querySelector('select');
 var button = document.querySelector('button');
-            var options = ['rock', 'paper', 'scissors'];
+
+var options = ['rock', 'paper', 'scissors'];
         
 //            window.onload = getPlayerChoice();
 //            window.onload = getComputerChoice();
-            button.addEventListener('click', playGame);
+select.addEventListener('input', playGame);
+button.addEventListener('click', playGame);
             
-            function getPlayerChoice(){
-                var choice = prompt('Choose an option!').toLowerCase();
-                
-                switch (choice){
-                    case 'rock':
-                        return 'rock';
-                        console.log('You chose "rock"');
-                        break;
-                    case 'scissors':
-                        return 'scissors';
-                        console.log('You chose "scissors"');
-                        break;
-                    case 'paper':
-                        return 'paper';
-                        console.log('You chose "paper"');
-                        break;
-                    default:
-                        getPlayerChoice();
-                }
-                
-                return choice;
-                
-            }
-            
-            function getComputerChoice(){
-                var randNum = Math.floor(Math.random() * 3);
-                var compChoice = options[randNum];
+function getPlayerChoice(){
+    var choice = prompt('Choose an option!').toLowerCase();
+
+    switch (choice){
+        case 'rock':
+            return 'rock';
+            console.log('You chose "rock"');
+            break;
+        case 'scissors':
+            return 'scissors';
+            console.log('You chose "scissors"');
+            break;
+        case 'paper':
+            return 'paper';
+            console.log('You chose "paper"');
+            break;
+        default:
+            getPlayerChoice();
+    }
+
+    return choice;
+
+}
+
+function getComputerChoice(){
+    var randNum = Math.floor(Math.random() * 3);
+    var compChoice = options[randNum];
 //                console.log(compChoice);
-                
-                return compChoice;
-            }
-            
-            // TODO: condense playGame func
-            // TODO: do something instead of console.logging
-            
-            function playGame(){
-                var player = getPlayerChoice();
-                var computer = getComputerChoice();
-                
-                console.log({player, computer});
-                
-                if (player === 'rock'){
-                    if (computer === 'rock'){
-                        console.log('Tie game!');
-                    } else if (computer === 'paper'){
-                        console.log('Computer wins...');
-                    } else {
-                        console.log('Congratulations, you win!');
-                    }
-                } else if (player === 'paper'){
-                    if (computer === 'paper'){
-                        console.log('Tie game!');
-                    } else if (computer === 'scissors'){
-                        console.log('Computer wins...');
-                    } else {
-                        console.log('Congratulations, you win!');
-                    }
-                } else {
-                    if (computer === 'scissors'){
-                        console.log('Tie game!');
-                    } else if (computer === 'rock'){
-                        console.log('Computer wins...');
-                    } else {
-                        console.log('Congratulations, you win!');
-                    }
-                }
-            }
-            
-            // TODO: create game(), a function to play 5 rounds and keeps track of the scores
-        
+
+    return compChoice;
+}
+
+// TODO: condense playGame func
+// TODO: do something instead of console.logging
+
+function playGame(){
+    var player = getPlayerChoice();
+    var computer = getComputerChoice();
+
+    console.log({player, computer});
+
+    if (player === 'rock'){
+        if (computer === 'rock'){
+            console.log('Tie game!');
+        } else if (computer === 'paper'){
+            console.log('Computer wins...');
+        } else {
+            console.log('Congratulations, you win!');
+        }
+    } else if (player === 'paper'){
+        if (computer === 'paper'){
+            console.log('Tie game!');
+        } else if (computer === 'scissors'){
+            console.log('Computer wins...');
+        } else {
+            console.log('Congratulations, you win!');
+        }
+    } else {
+        if (computer === 'scissors'){
+            console.log('Tie game!');
+        } else if (computer === 'rock'){
+            console.log('Computer wins...');
+        } else {
+            console.log('Congratulations, you win!');
+        }
+    }
+}
+
+// TODO: create game(), a function to play 5 rounds and keeps track of the scores
