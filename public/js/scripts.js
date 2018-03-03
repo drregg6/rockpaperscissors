860bba0dd368eeebba18ec4483a0d07d7e915b50
Dave <drregg6@gmail.com>
2018-03-03 13:38:32 -0500

    updating playGame func

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -9,79 +9,75 @@ let playerScore = parseInt(document.querySelector('.player-score').textContent);
 let computerScore = parseInt(document.querySelector('.computer-score').textContent);
 
 
-let computer = 'rock';
-let player = select.options[select.selectedIndex].textContent.toLowerCase();
+let computerChoice = 'rock';
+let playerChoice = select.options[select.selectedIndex].textContent.toLowerCase();
 
 
 // change player's choice on selection
 select.addEventListener('input', getPlayerChoice);
 
 // play game on button click
-//playBtn.addEventListener('click', playGame);
+playBtn.addEventListener('click', playGame);
 
 // reset game on button click
 //resetBtn.addEventListener('click', resetGame);
 
 
 function getPlayerChoice() {
-    player = select.options[select.selectedIndex].textContent.toLowerCase();
+    playerChoice = select.options[select.selectedIndex].textContent.toLowerCase();
 }
 
 function getComputerChoice() {
     var randNum = Math.floor(Math.random() * 3);
-    computer = options[randNum];
+    computerChoice = options[randNum];
     
-    return computer;
+    return computerChoice;
 }
 
 // TODO: condense playGame func
 // TODO: do something instead of console.logging
 
 function playGame() {
-    computer = getComputerChoice();
+    computerChoice = getComputerChoice();
     
     console.log({
-        player,
-        computer
+        playerChoice,
+        computerChoice
     });
+    
+    console.log(getWinner(computerChoice, playerChoice));
+}
 
+function getWinner(comp, player){
+    var winner = '';
+    
     if (player === 'rock') {
-        if (computer === 'rock') {
-            console.log('Tie game!');
-        } else if (computer === 'paper') {
-            console.log('Computer wins...');
+        if (comp === 'rock') {
+            winner = 'tie';
+        } else if (comp === 'paper') {
+            winner = 'computer';
         } else {
-            console.log('Congratulations, you win!');
+            winner = 'player';
         }
     } else if (player === 'paper') {
-        if (computer === 'paper') {
-            console.log('Tie game!');
-        } else if (computer === 'scissors') {
-            console.log('Computer wins...');
+        if (comp === 'paper') {
+            winner = 'tie';
+        } else if (comp === 'scissors') {
+            winner = 'computer';
         } else {
-            console.log('Congratulations, you win!');
+            winner = 'player';
         }
     } else {
-        if (computer === 'scissors') {
-            console.log('Tie game!');
+        if (comp === 'scissors') {
+            winner = 'tie';
         } else if (computer === 'rock') {
-            console.log('Computer wins...');
+            winner = 'computer';
         } else {
-            console.log('Congratulations, you win!');
+            winner = 'player';
         }
     }
+    
+    return winner;
 }
 
 // TODO: create game(), a function to play 5 rounds and keeps track of the scores
-
-
-//function getElText(element) {
-//    var elt = document.querySelector('select');
-//
-//    if (elt.selectedIndex == -1)
-//        return null;
-//
-//    return elt.options[elt.selectedIndex].text;
-//}
-//
-//console.log(getElText(select));
